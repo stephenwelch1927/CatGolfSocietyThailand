667a4be037b201716fbf2e5d384a0ceff40bdf93
stephenwelch1927 <stephenwelch1927@yahoo.com>
2024-06-14 15:05:08 +0700

    Added in Contact Form Icon to header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,19 +3,23 @@ import Golf from "../public/Golf-PNG-Free-Download.png";
 import Image from "next/image";
 import Link from "next/link";
 import { MdOutlinePhone } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
     <div>
       <header className="relative w-full h-20 bg-slate-800">
-      <div className="flex items-start">
-      <MdOutlinePhone size={60} className="flex fill-white py-4"/>
-      <p className="text-white py-5">+66 801567162</p>
+      <div className="flex items-start ">
+      <MdOutlinePhone size={60} className="flex fill-white py-4 space-x-30"/>
+      <p className="text-white py-5 ">+66 801567162</p>
+      <FaWpforms size={30} className="flex fill-white float-right ml-5 mt-4" />
+      <p className="text-white py-5 ml-1">Contact Form</p>
+
       </div>
       </header>
       <nav className="relative w-full h-25 bg-gray-200">
-        <div className="flex items-center h-full w-full px-4 2xl:px-16">
+        <div className="flex items-center h-full w-full px-4 2xl:px-16 text-center sm:text-left">
           <Link href="/">
           <Image
             src={Golf}
@@ -52,6 +56,21 @@ const Navbar = () => {
             <p class="peer px-5 py-2 hover:bg-gray-300 text-slate-950 text-xl">
             Tournaments |
             </p>
+            <div
+              class="hidden peer-hover:flex hover:flex
+         w-[200px]
+         flex-col bg-white drop-shadow-lg absolute z-10"
+            >
+              <Link className="px-5 py-3 hover:bg-gray-200"  href="/thailand">
+                Bangkok Pro Classic
+              </Link>
+              <a class="px-5 py-3 hover:bg-gray-200" href="#">
+                Cambodia Angkor Open
+              </a>
+              <a class="px-5 py-3 hover:bg-gray-200" href="#">
+                Vietnam Saigon Tour
+              </a>
+            </div>
             </div>
         </div>
       </nav>
